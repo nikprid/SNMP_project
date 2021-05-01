67afe2d6f47ca95ad4f08255722864ab7f75396c
nikprid <pridannikov-nikita@mail.ru>
2021-05-01 22:09:03 +0300

    Agent coded

--- snmp_agent.py
+++ snmp_agent.py
@@ -1,22 +1,46 @@
-from pysnmp.hlapi import *
+from pysnmp.entity import engine, config
+from pysnmp.entity.rfc3413 import cmdrsp, context
+from pysnmp.carrier.asyncore.dgram import udp
 
-iterator = sendNotification(
-    SnmpEngine(),
-    CommunityData('public', mpModel=1),
-    UdpTransportTarget(('localhost', 162)),
-    ContextData(),
-    'trap',
-    NotificationType(
-        ObjectIdentity('1.3.6.1.6.3.1.1.5.2')
-    ).addVarBinds(
-        ('1.3.6.1.6.3.1.1.4.3.0', '1.3.6.1.4.1.20408.4.1.1.2'),
-        ('1.3.6.1.2.1.1.1.0', OctetString('my system'))
-    ).loadMibs(
-        'SNMPv2-MIB'
-    )
+# Create SNMP engine with autogenernated engineID and pre-bound
+# to socket transport dispatcher
+snmpEngine = engine.SnmpEngine()
+
+# Transport setup
+
+# UDP over IPv4
+config.addTransport(
+    snmpEngine,
+    udp.DOMAIN_NAME,
+    udp.UdpTransport().openServerMode(('127.0.0.1', 161))
 )
 
-errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+# SNMPv1 setup
 
-if errorIndication:
-    print(errorIndication)
+# SecurityName <-> CommunityName mapping.
+# Here we configure two distinct CommunityName's to control read and write
+# operations.
+config.addV1System(snmpEngine, 'my-read-area', 'public')
+config.addV1System(snmpEngine, 'my-write-area', 'private')
+
+# Allow full MIB access for this user / securityModels at VACM
+config.addVacmUser(snmpEngine, 1, 'my-read-area', 'noAuthNoPriv', (1, 3, 6, 1, 2, 1))
+config.addVacmUser(snmpEngine, 1, 'my-write-area', 'noAuthNoPriv', (1, 3, 6, 1, 2, 1), (1, 3, 6, 1, 2, 1))
+
+# Get default SNMP context this SNMP engine serves
+snmpContext = context.SnmpContext(snmpEngine)
+
+# Register SNMP Applications at the SNMP engine for particular SNMP context
+cmdrsp.GetCommandResponder(snmpEngine, snmpContext)
+cmdrsp.SetCommandResponder(snmpEngine, snmpContext)
+cmdrsp.NextCommandResponder(snmpEngine, snmpContext)
+
+# Register an imaginary never-ending job to keep I/O dispatcher running forever
+snmpEngine.transportDispatcher.jobStarted(1)
+
+# Run I/O dispatcher which would receive queries and send responses
+try:
+    snmpEngine.transportDispatcher.runDispatcher()
+
+finally:
+    snmpEngine.transportDispatcher.closeDispatcher()
