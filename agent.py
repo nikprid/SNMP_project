ad3bf10ccee076a9d4d25c3636ffb8345fbff791
nikprid <pridannikov-nikita@mail.ru>
2021-05-08 16:32:54 +0300

    Final result

--- agent.py
+++ agent.py
@@ -1,46 +1,92 @@
 from pysnmp.entity import engine, config
 from pysnmp.entity.rfc3413 import cmdrsp, context
-from pysnmp.carrier.asyncore.dgram import udp
+from pysnmp.carrier.asynsock.dgram import udp
+import platform
 
-# Create SNMP engine with autogenernated engineID and pre-bound
-# to socket transport dispatcher
 snmpEngine = engine.SnmpEngine()
 
-# Transport setup
+print('Конфигурация агента...')
 
-# UDP over IPv4
-config.addTransport(
+# Transport setup UDP over IPv4
+IP = '127.0.0.1'
+PORT = 1234
+
+config.addSocketTransport(
     snmpEngine,
     udp.domainName,
-    udp.UdpTransport().openServerMode(('127.0.0.1', 1234))
+    udp.UdpTransport().openServerMode((IP, PORT))
 )
 
-# SNMPv1 setup
+# SNMPv1/2c setup
+# SecurityName <-> CommunityName mapping
+config.addV1System(snmpEngine, 'my-area', 'public')
 
-# SecurityName <-> CommunityName mapping.
-# Here we configure two distinct CommunityName's to control read and write
-# operations.
-config.addV1System(snmpEngine, 'my-read-area', 'public')
-config.addV1System(snmpEngine, 'my-write-area', 'private')
+# SNMPv3/USM setup 
+# user: usr-md5-des, auth: MD5, priv DES
+config.addV3User(
+    snmpEngine, 'usr-md5-des',
+    config.usmHMACMD5AuthProtocol, 'authkey1',
+    config.usmDESPrivProtocol, 'privkey1'
+)
 
-# Allow full MIB access for this user / securityModels at VACM
-config.addVacmUser(snmpEngine, 1, 'my-read-area', 'noAuthNoPriv', (1, 3, 6, 1, 2, 1))
-config.addVacmUser(snmpEngine, 1, 'my-write-area', 'noAuthNoPriv', (1, 3, 6, 1, 2, 1), (1, 3, 6, 1, 2, 1))
+# user: usr-none-none, auth: NONE, priv NONE
+config.addV3User(
+    snmpEngine, 'usr-none-none'
+)
 
-# Get default SNMP context this SNMP engine serves
+# user: usr-md5-none, auth: MD5, priv NONE
+config.addV3User(
+    snmpEngine, 'usr-md5-none',
+    config.usmHMACMD5AuthProtocol, 'authkey1'
+)
+
+# user: usr-sha-aes128, auth: SHA, priv AES
+config.addV3User(
+    snmpEngine, 'usr-sha-aes128',
+    config.usmHMACSHAAuthProtocol, 'authkey1',
+    config.usmAesCfb128Protocol, 'privkey1'
+)
+
+# Access control (VACM) setup
+# allow full MIB access for each user
+config.addVacmUser(snmpEngine, 2, 'my-area', 'noAuthNoPriv', (1,3,6), (1,3,6)) 
+config.addVacmUser(snmpEngine, 3, 'usr-md5-des', 'authPriv', (1,3,6), (1,3,6)) 
+config.addVacmUser(snmpEngine, 3, 'usr-none-none', 'noAuthNoPriv', (1,3,6), (1,3,6)) 
+config.addVacmUser(snmpEngine, 3, 'usr-md5-none', 'authNoPriv', (1,3,6), (1,3,6)) 
+config.addVacmUser(snmpEngine, 3, 'usr-sha-aes128', 'authPriv', (1,3,6), (1,3,6)) 
+
+#
+# CommandResponder could serve multiple independent MIB trees
+# selected by ContextName parameter. The default ContextName is
+# an empty string, this is where SNMP engine's LCD also lives.
+#
 snmpContext = context.SnmpContext(snmpEngine)
 
-# Register SNMP Applications at the SNMP engine for particular SNMP context
+# Add our own Managed Object to default MIB tree
+# Get a reference to defautl MIB tree instrumentation
+mibInstrumController = snmpContext.getMibInstrum('')
+
+# Create and put on-line my managed object
+u=platform.uname()
+sysDescr, = mibInstrumController.mibBuilder.importSymbols('SNMPv2-MIB', 'sysDescr')
+MibScalarInstance, = mibInstrumController.mibBuilder.importSymbols('SNMPv2-SMI', 'MibScalarInstance')
+sysDescrInstance = MibScalarInstance(sysDescr.name, (0,), sysDescr.syntax.clone(u.system+' '+u.node+' '+u.release+' '+u.version+' '+u.processor))
+mibInstrumController.mibBuilder.exportSymbols('PYSNMP-EXAMPLE-MIB', sysDescrInstance)  # add anonymous Managed Object Instance
+
+# Register SNMP Applications at the SNMP engine
 cmdrsp.GetCommandResponder(snmpEngine, snmpContext)
 cmdrsp.SetCommandResponder(snmpEngine, snmpContext)
 cmdrsp.NextCommandResponder(snmpEngine, snmpContext)
+cmdrsp.BulkCommandResponder(snmpEngine, snmpContext)
 
-# Register an imaginary never-ending job to keep I/O dispatcher running forever
-snmpEngine.transportDispatcher.jobStarted(1)
+print('Конфигурация агента прошла успешно.')
+print('Агент запущен по ip-адресу {} на порту {}.'.format(IP, PORT))
+
+snmpEngine.transportDispatcher.jobStarted(1) 
 
 # Run I/O dispatcher which would receive queries and send responses
 try:
     snmpEngine.transportDispatcher.runDispatcher()
-
-finally:
+except:
     snmpEngine.transportDispatcher.closeDispatcher()
+    print('\nАгент остановлен')
--- command_app.py
+++ command_app.py
@@ -2,21 +2,24 @@ from pysnmp.hlapi import *
 
 iterator = getCmd(
     SnmpEngine(),
-    CommunityData('public', mpModel=0),
-    UdpTransportTarget(('localhost', 1234)),
+    CommunityData('public', mpModel=1),
+    UdpTransportTarget(('127.0.0.1', 161)),
     ContextData(),
-    ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))
+    ObjectType(ObjectIdentity('1.3.6.1.2.1.1.2.0'))
 )
 
-errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+for errorIndication, errorStatus, errorIndex, varBinds in iterator:
 
-if errorIndication:
-    print(errorIndication)
+    if errorIndication:
+        print(errorIndication)
+        break
 
-elif errorStatus:
-    print('%s at %s' % (errorStatus.prettyPrint(),
-                        errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+    elif errorStatus:
+        print('%s at %s' % (errorStatus.prettyPrint(),
+                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+        break
 
-else:
-    for varBind in varBinds:
-        print(' = '.join([x.prettyPrint() for x in varBind]))
+    else:
+        for varBind in varBinds:
+            print(' = '.join([x.prettyPrint() for x in varBind]))
+
