a37f20c16641be34cd5350786c3423bf8829d32a
nikprid <pridannikov-nikita@mail.ru>
2021-05-08 16:34:41 +0300

    Update command_app.py

--- command_app.py
+++ command_app.py
@@ -3,7 +3,7 @@ from pysnmp.hlapi import *
 iterator = getCmd(
     SnmpEngine(),
     CommunityData('public', mpModel=1),
-    UdpTransportTarget(('127.0.0.1', 161)),
+    UdpTransportTarget(('127.0.0.1', 1234)),
     ContextData(),
     ObjectType(ObjectIdentity('1.3.6.1.2.1.1.2.0'))
 )
